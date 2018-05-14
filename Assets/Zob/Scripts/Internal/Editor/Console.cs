aa1e788f31aadfed41308ffb2b903095c4158140
vthemereau <vthemereau@asobostudio.com>
2018-05-14 14:07:16 +0200

    focus on scroll / log entries

--- Assets/Zob/Scripts/Internal/Editor/Console.cs
+++ Assets/Zob/Scripts/Internal/Editor/Console.cs
@@ -1,59 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 namespace Zob.Internal.Editor
 {
-    public interface IPosition
-    {
-        Rect Position { get; }
-    }
-
-    public abstract class ChildGUI : IPosition
-    {
-        protected Rect _position;
-        protected IPosition _parent;
-        public bool Repaint { get; protected set; }
-
-        public ChildGUI(IPosition parent)
-        {
-            _parent = parent;
-            _position = _parent.Position;
-        }
-
-        public Rect Position { get { return _position; } }
-
-        public abstract void OnGUI();
-    }
-
-    public class SeparationLine : ChildGUI
-    {
-        protected Texture2D _greyTex;
-
-        public SeparationLine(IPosition parent) : base(parent)
-        {
-            _greyTex = new Texture2D(1, 1);
-            _greyTex.SetPixel(0, 0, Color.cyan);
-            _greyTex.Apply();
-            _position.y = 50;
-            _position.height = 1;
-        }
-
-        public override void OnGUI()
-        {
-            if (Event.current.type == EventType.MouseDrag)
-            {
-                _position.y = Event.current.mousePosition.y;
-                Repaint = true;
-            }
-            GUI.DrawTexture(_position, _greyTex);
-            var overRect = _position;
-            overRect.height = overRect.height + 3;
-            EditorGUIUtility.AddCursorRect(overRect, MouseCursor.ResizeVertical);
-        }
-    }
-
-    public class Console : EditorWindow, IPosition
+    public class Console : EditorWindow
     {
         private Vector2 scrollPos;
         private int count = 0;
@@ -63,15 +15,11 @@ namespace Zob.Internal.Editor
         private float _rowHeight = 30f;
         private bool _initialized = false;
         private ConsoleConfig _config;
-        private SeparationLine _seperationLine;
 
-        Rect IPosition.Position
-        {
-            get
-            {
-                return position;
-            }
-        }
+        private Texture2D _row1;
+        private Texture2D _row2;
+
+        private List<LogEntry> _logEntries = new List<LogEntry>();
 
         // Add menu named "My Window" to the Window menu
         [MenuItem("Window/ZobConsole")]
@@ -93,57 +41,47 @@ namespace Zob.Internal.Editor
             wantsMouseMove = true;
             titleContent = new GUIContent("ZobConsole");
             _config = ConsoleConfig.Load();
-            _seperationLine = new SeparationLine(this);
             _initialized = true;
+            _row1 = new Texture2D(1, 1);
+            _row1.SetPixel(0, 0, new Color32(220, 220, 220, 255));
+            _row1.Apply();
+
+            _row2 = new Texture2D(1, 1);
+            _row2.SetPixel(0, 0, new Color32(200, 200, 200, 255));
+            _row2.Apply();
+
+            for (int i = 0; i < 100; ++i)
+            {
+                var entry = new LogEntry();
+                entry.args = null;
+                entry.domain = "log-console";
+                entry.format = "this is a log entry " + UnityEngine.Random.Range(0, 10000);
+                entry.level = LogLevel.Info;
+                entry.timestamp = DateTime.Now;
+                _logEntries.Add(entry);
+            }
         }
 
         void OnGUI()
         {
             InitializeOnce();
-            //EditorGUILayout.BeginHorizontal();
-            //scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
-            //GUILayout.BeginArea(new Rect(0, 0, texSize, texSize), Texture2D.whiteTexture);
-            //GUILayout.EndArea();
-            //for (int i = 0; i < count; ++i)
-            //{
-            //    GUILayout.Label(i.ToString());
-            //}
-            //EditorGUILayout.EndScrollView();
-            //EditorGUILayout.EndHorizontal();
-            //if (GUILayout.Button("+10"))
-            //    count += 10;
-            //if (GUILayout.Button("+10 tex"))
-            //    texSize += 10;
-            //EditorGUILayout.LabelField(scrollPos.ToString());
-            //_rect = EditorGUILayout.RectField(_rect);
-            //GUI.Label(_rect, "test");
-            //EditorGUILayout.RectField("position", _window.position);
-            //EditorGUILayout.Vector2Field("delta", Event.current.delta);
-            //for (int i = 0; i < 10; ++i)
-            //{
-            //    GUI.DrawTexture(new Rect(0, i * _rowHeight, _window.position.width - GUI.skin.verticalScrollbar.fixedWidth, _rowHeight), Texture2D.whiteTexture);
-            //}
+            var logEntryPosition = new Rect(0, 50, position.width, position.height - 100);
+            RenderLogEntries(logEntryPosition);
+        }
 
-            if (Event.current.type != EventType.Layout)
-                Debug.Log("event=" + Event.current.type);
-
-            _seperationLine.OnGUI();
-            if (_seperationLine.Repaint)
+        void RenderLogEntries(Rect position)
+        {
+            var scrollbarPosition = new Rect(position.x + position.width - GUI.skin.verticalScrollbar.fixedWidth, position.y, GUI.skin.verticalScrollbar.fixedWidth, position.height);
+            _scrollValue = GUI.VerticalScrollbar(scrollbarPosition, _scrollValue, 1, 10f, 0f);
+            for (int i = 0; i < 2; ++i)
             {
-                Repaint();
+                Texture2D rowText = _row1;
+                if (i % 2 == 0)
+                {
+                    rowText = _row2;
+                }
+                GUI.DrawTexture(new Rect(position.x, position.y + i * 20f, position.width - GUI.skin.verticalScrollbar.fixedWidth, 20f), rowText);
             }
-            EditorGUILayout.Vector2Field("mouse", Event.current.mousePosition);
-            GUI.DrawTexture(new Rect(0, 0, 100, 100), Texture2D.blackTexture);
-            //GUI.DrawTexture(new Rect(50, 50, 150, 150), Texture2D.blackTexture);
-
-
-            //if (Event.current.type == EventType.ScrollWheel)
-            //{
-            //    _scrollValue += Event.current.delta.y;
-            //    Mathf.Clamp(_scrollValue, 0, 10);
-            //    Repaint();
-            //}
-
         }
     }
 }
