1b167cc326964e581ca4957823298ba0f2ebc6da
Vincent Themereau <vincent@chibougamau.org>
2018-06-13 22:55:08 +0200

    wip search bar

--- Assets/Zob/Scripts/Internal/Editor/Console.cs
+++ Assets/Zob/Scripts/Internal/Editor/Console.cs
@@ -25,6 +25,8 @@ namespace Zob.Internal.Editor
         private Dictionary<string, string> _debug = new Dictionary<string, string>();
         private List<Rect> _entriesRect = new List<Rect>();
         private int _selectedRowEntryIndex = -1;
+        private UnityEditor.IMGUI.Controls.SearchField _searchField;
+        private string _searchFieldResult = "...";
 
         // Add menu named "My Window" to the Window menu
         [MenuItem("Window/ZobConsole")]
@@ -49,7 +51,7 @@ namespace Zob.Internal.Editor
             _config = ConsoleConfig.Load();
             _initialized = true;
             _row1 = new Texture2D(1, 1);
-            _row1.SetPixel(0, 0, new Color32(55, 55, 55, 255));
+            _row1.SetPixel(0, 0, new Color32(155, 155, 155, 255));
             _row1.Apply();
 
             _row2 = new Texture2D(1, 1);
@@ -57,8 +59,11 @@ namespace Zob.Internal.Editor
             _row2.Apply();
 
             _selectedRow = new Texture2D(1, 1);
-            _selectedRow.SetPixel(0, 0, Color.blue);
+            _selectedRow.SetPixel(0, 0, new Color32(62, 95, 150, 255));
             _selectedRow.Apply();
+
+            _searchField = new UnityEditor.IMGUI.Controls.SearchField();
+            _searchField.autoSetFocusOnFindCommand = true;
 
             for (int i = 0; i < 100; ++i)
             {
@@ -75,11 +80,19 @@ namespace Zob.Internal.Editor
         protected void OnGUI()
         {
             InitializeOnce();
-            var logEntryPosition = new Rect(0, 50, position.width, position.height - 100);
-            RenderLogEntries(logEntryPosition);
+
             foreach (var kv in _debug)
             {
                 EditorGUILayout.LabelField(kv.Key + "=" + kv.Value);
+            }
+
+            if (Event.current.type != EventType.Layout)
+            {
+                var searchFieldPosition = new Rect(100, 2, position.width - 120, 20f);
+                _searchFieldResult = _searchField.OnToolbarGUI(searchFieldPosition, _searchFieldResult);
+
+                var logEntryPosition = new Rect(0, 50, position.width, position.height - 100);
+                RenderLogEntries(logEntryPosition);
             }
             if (Event.current.keyCode == KeyCode.Space && Event.current.type == EventType.KeyDown)
             {
@@ -95,21 +108,21 @@ namespace Zob.Internal.Editor
 
         protected void RenderLogEntries(Rect position)
         {
-            int rowCount = Mathf.RoundToInt(position.height / _rowHeight);
+            int rowCount = Mathf.CeilToInt(position.height / _rowHeight);
             _debug["rowCount"] = rowCount.ToString();
             var scrollbarPosition = new Rect(position.x + position.width - GUI.skin.verticalScrollbar.fixedWidth, position.y, GUI.skin.verticalScrollbar.fixedWidth, position.height);
-            _scrollValue = GUI.VerticalScrollbar(scrollbarPosition, _scrollValue, 1, 0f, _logEntries.Count - rowCount);
+            _scrollValue = GUI.VerticalScrollbar(scrollbarPosition, _scrollValue, 1, 0f, _logEntries.Count - rowCount + 1);
             _debug["scroll"] = _scrollValue.ToString();
 
             _entriesRect.Clear();
-            for (int rowIndex = 0; rowIndex <= rowCount; ++rowIndex)
+            for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
             {
                 Texture2D rowTexture = _row1;
                 if (((int)_scrollValue + rowIndex) % 2 == 0)
                 {
                     rowTexture = _row2;
                 }
-                if (rowIndex == _selectedRowEntryIndex)
+                if (rowIndex + (int)_scrollValue == _selectedRowEntryIndex)
                 {
                     rowTexture = _selectedRow;
                 }
@@ -138,6 +151,7 @@ namespace Zob.Internal.Editor
                 if (_entriesRect[i].Contains(Event.current.mousePosition))
                 {
                     _selectedRowEntryIndex = i + (int)_scrollValue;
+                    Repaint();
                     break;
                 }
             }
