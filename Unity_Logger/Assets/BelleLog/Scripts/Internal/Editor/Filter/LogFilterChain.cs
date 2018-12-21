40b29d7e45b22e0625814a8fa8d3af4e063447b4
Vincent Themereau <vincent@chibougamau.org>
2018-12-21 21:10:46 +0100

    add missing file

---
+++ Unity_Logger/Assets/BelleLog/Scripts/Internal/Editor/Filter/LogFilterChain.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace BelleLog.Internal.Editor.Filter
+{
+    internal class LogFilterChain
+    {
+        private List<ILogFilter> _filters = new List<ILogFilter>();
+
+        public void AddFilter(ILogFilter filter)
+        {
+            if (!_filters.Contains(filter))
+            {
+                _filters.Add(filter);
+            }
+        }
+
+        public void RemoveFilter(ILogFilter filter)
+        {
+            _filters.Remove(filter);
+        }
+
+        public void ResetAll()
+        {
+            for (int f = 0; f < _filters.Count; ++f)
+            {
+                _filters[f].Reset();
+            }
+        }
+
+        public LogFilterAction Apply(LogEntry entry)
+        {
+            LogFilterTermination termination;
+            LogFilterAction action = LogFilterAction.Accept;
+            for (int f = 0; f < _filters.Count; ++f)
+            {
+                if (!_filters[f].Enable)
+                {
+                    continue;
+                }
+                _filters[f].Apply(entry, ref action, out termination);
+                if (termination == LogFilterTermination.Stop)
+                {
+                    break;
+                }
+            }
+            return action;
+        }
+    }
+}
